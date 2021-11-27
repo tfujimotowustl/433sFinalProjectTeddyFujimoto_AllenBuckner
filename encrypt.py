393b635f607d122a4c621599408e1baf3bf97501
tfujimotowustl <59811628+tfujimotowustl@users.noreply.github.com>
2021-11-27 14:08:52 -0600

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,7 @@ keyFile=sys.argv[2]
 f=open("message.txt",'w')
 f.write(message)
 f.close()
-encryptCMD="openssl rsautl -encrypt -inkey "+keyFile+" -pubin -in message.txt -$
+encryptCMD="openssl rsautl -encrypt -inkey "+keyFile+" -pubin -in message.txt -out cipher.bin$
 k=os.popen(encryptCMD).readlines()
 rmCmd="rm message.txt"
 l=os.popen(encryptCMD).readlines()
