d22296363d7f0e35f4fba6a85e85d4ac82aea6c4
tfujimotowustl <59811628+tfujimotowustl@users.noreply.github.com>
2021-11-27 14:09:51 -0600

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -2,7 +2,7 @@
 #https://codingbee.net/centos/openssl-demo-encrypting-decrypting-files-using-bo$
 #Used to generate two keys using two different commands. Needed for key exchange
 import os
-genPrivateKeyCMD="openssl genpkey -algorithm rsa -out privateKey.pem -pkeyopt r$
+genPrivateKeyCMD="openssl genpkey -algorithm rsa -out privateKey.pem -pkeyopt rsa_keygen_bits:256 $
 k=os.popen(genPrivateKeyCMD).readlines()
 publicKeyGen="openssl rsa -pubout -in privateKey.pem -out publicKey.pem"
 k=os.popen(publicKeyGen).readlines()
